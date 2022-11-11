13847c8f08b282fa7d1d786229c5b919680a2c67
William Chalk <william@printedproducts.com>
2022-11-11 17:05:34 -0500

    added dropdown

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -5,3 +5,13 @@ function showToast({ message }) {
     var myToast = new bootstrap.Toast(toast, { autohide: true });
     myToast.show();
   }
+
+  var checkList = document.getElementById('tagList');
+  checkList.getElementsByClassName('anchor')[0].onclick = function(evt){
+    if(checkList.classList.contains('visible')){
+      checkList.classList.remove('visible');
+    }
+    else{
+      checkList.classList.add('visible');
+    }
+  }
