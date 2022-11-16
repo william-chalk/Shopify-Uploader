aeff981d57b7ec0e996c69b1bc5b6ebd43bccf65
William Chalk <3halk1@gmail.com>
2022-11-15 19:57:27 -0500

    tag dropdown

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,39 +1,49 @@
 function showToast({ message }) {
-    var toastBody = document.querySelector('.toast-body');
-    toastBody.textContent = message;
-    var toast = document.querySelector('#liveToast');
-    var myToast = new bootstrap.Toast(toast, { autohide: true });
-    myToast.show();
+  var toastBody = document.querySelector(".toast-body");
+  toastBody.textContent = message;
+  var toast = document.querySelector("#liveToast");
+  var myToast = new bootstrap.Toast(toast, { autohide: true });
+  myToast.show();
+}
+
+var checkList = document.getElementById("tagList");
+checkList.getElementsByClassName("anchor")[0].onclick = function (evt) {
+  if (checkList.classList.contains("visible")) {
+    checkList.classList.remove("visible");
+  } else {
+    checkList.classList.add("visible");
   }
+};
 
-  var checkList = document.getElementById('tagList');
-  checkList.getElementsByClassName('anchor')[0].onclick = function(evt){
-    if(checkList.classList.contains('visible')){
-      checkList.classList.remove('visible');
+function getTags() {
+  const tag = $(".tag");
+  const checkedTags = [];
+  tag.each(function () {
+    if ($(this).is(":checked")) {
+      checkedTags.push(this.value);
     }
-    else{
-      checkList.classList.add('visible');
-    }
-  }
+  });
+  console.log(checkedTags);
+  return checkedTags;
+}
 
-
-
-function submitInfo(event){
+function submitInfo(event) {
   event.preventDefault();
 
   const templateName = document.querySelector("#template-name").value;
-  const templateDescripton = document.querySelector("#template-description").value;
+  const templateDescripton = document.querySelector(
+    "#template-description"
+  ).value;
   const templateWeight = document.querySelector("#weight").value;
   const templateCost = document.querySelector("#cost").value;
-  const templateTags = document.querySelector("input[class='tags']:checked").value;
-  const templateStyle = document.querySelector("#style").value;
+  const templateTags = getTags();
+  // const templateStyle = document.querySelector("#style").value;
   const templateStatus = document.querySelector("input[name='status']").value;
   const templatePublish = document.querySelector("input[name=publish]").value;
 
-  const test = `${templateName}  ${templateDescripton}  ${templateWeight}  ${templateCost}  ${templateTags}  ${templateStyle}  ${templateStatus}  ${templatePublish}`;
+  const test = ` ${templateTags}`;
 
-  console.log(JSON.stringify(test));
-
+  console.log(test);
 }
 
-document.querySelector("#submit-btn").addEventListener("click",submitInfo);
+document.querySelector("#submit-btn").addEventListener("click", submitInfo);
